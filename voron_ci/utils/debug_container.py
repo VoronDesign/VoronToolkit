60925f5389fd487026f5fffee8e479eaa4322120
Florian Heilmann <Florian.Heilmann@gmx.net>
2023-11-29 16:34:58 +0100

    fix: Fix output of debug-container

--- voron_ci/utils/debug_container.py
+++ voron_ci/utils/debug_container.py
@@ -6,6 +6,7 @@ logger = init_logging(__name__)
 
 
 def print_container_info() -> None:
+    logger.setLevel("INFO")
     logger.info("## Debugging environment variables:\n")
     logger.info("\tGITHUB_OUTPUT=%s", os.environ.get("GITHUB_OUTPUT", ""))
     logger.info("\tGITHUB_STEP_SUMMARY=%s", os.environ.get("GITHUB_STEP_SUMMARY", ""))
